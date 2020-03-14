4a0c30dca214e62720632318cefcc358887fc962
Nicholas Solin <solin.nick@gmail.com>
2020-03-14 17:10:16 -0500

    Switch back to Scopophobia now that it has released

--- FriendlyFireAutoban/Extensions.cs
+++ FriendlyFireAutoban/Extensions.cs
@@ -6,8 +6,8 @@ namespace FriendlyFireAutoban
 		//When sending RaReply's, you need to identify the 'source' of the message with a string followed by '#' at the start of the message, otherwise the message will not be sent
 		public static void RAMessage(this CommandSender sender, string message, bool success = true) =>
 			sender.RaReply("FriendlyFireAutoban#" + message, success, true, string.Empty);
-
-		public static void Broadcast(this ReferenceHub rh, uint time, string message) => 
-			rh.GetComponent<Broadcast>().TargetAddElement(rh.scp079PlayerScript.connectionToClient, message, time, false);
+		
+		public static void Broadcast(this ReferenceHub rh, ushort time, string message) =>
+			rh.GetComponent<Broadcast>().TargetAddElement(rh.scp079PlayerScript.connectionToClient, message, time, global::Broadcast.BroadcastFlags.Normal);
 	}
 }
